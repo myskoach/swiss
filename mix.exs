6261df4f79e1e85962bb8f444449fe519f011a07
João Ferreira <jmnsferreira@gmail.com>
2019-12-03 12:51:46 +0000

    Adds publishing info
    Signed-off-by: João Ferreira <jmnsferreira@gmail.com>

--- mix.exs
+++ mix.exs
@@ -4,11 +4,15 @@ defmodule Swiss.MixProject do
   def project do
     [
       app: :swiss,
-      version: "0.1.0",
+      description: "Swiss is a bundle of extensions to the standard lib.",
+      version: "1.0.0",
       elixir: "~> 1.8",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      package: package(),
+      homepage_url: "https://github.com/myskoach/swiss",
+      source_url: "https://github.com/myskoach/swiss"
     ]
   end
 
@@ -23,8 +27,17 @@ defmodule Swiss.MixProject do
   defp elixirc_paths(:test), do: ["lib", "test/support"]
   defp elixirc_paths(_), do: ["lib"]
 
+  defp package do
+    %{
+      licenses: ["Apache-2.0"],
+      links: %{
+        "GitHub" => "https://github.com/myskoach/swiss"
+      }
+    }
+  end
+
   # Run "mix help deps" to learn about dependencies.
   defp deps do
-    []
+    [{:ex_doc, ">= 0.0.0", only: :dev, runtime: false}]
   end
 end
