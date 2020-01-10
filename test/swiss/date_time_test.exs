8743cbea8006641d0a663a0f91fc14b182817905
João Ferreira <jmnsferreira@gmail.com>
2020-01-10 16:14:17 +0000

    Adds DateTime.max and DateTime.min
    Signed-off-by: João Ferreira <jmnsferreira@gmail.com>

--- test/swiss/date_time_test.exs
+++ test/swiss/date_time_test.exs
@@ -1,5 +1,6 @@
 defmodule Swiss.DateTimeTest do
   use ExUnit.Case, async: true
+  doctest Swiss.DateTime
 
   describe "second_utc_now/0" do
     test "returns the current UTC date with second precision" do
