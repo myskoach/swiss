8541b6e791adbaed5818ae799d5edf50aae5235d
João Ferreira <jmnsferreira@gmail.com>
2020-04-17 13:48:19 +0100

    Adds Map.indif_fetch\!/2
    Signed-off-by: João Ferreira <jmnsferreira@gmail.com>

--- lib/swiss/map.ex
+++ lib/swiss/map.ex
@@ -13,7 +13,7 @@ defmodule Swiss.Map do
       iex> Swiss.Map.from_struct(%{__struct__: SomeStruct, life: 42})
       %{life: 42}
   """
-  @spec from_struct(struct | nil) :: Map.t | nil
+  @spec from_struct(struct | nil) :: Map.t() | nil
   def from_struct(nil), do: nil
   def from_struct(struct), do: Map.from_struct(struct)
 
@@ -27,7 +27,7 @@ defmodule Swiss.Map do
       iex> Swiss.Map.to_string_keys(%{"life" => 42, death: 27})
       %{"life" => 42, "death" => 27}
   """
-  @spec to_string_keys(Map.t) :: Map.t
+  @spec to_string_keys(Map.t()) :: Map.t()
   def to_string_keys(map) do
     map
     |> Map.to_list()
@@ -36,5 +36,37 @@ defmodule Swiss.Map do
       entry -> entry
     end)
     |> Enum.into(%{})
+  end
+
+  @doc """
+  Fetches a value from a map with indifferent access, i.e. given an atom,
+  returns the value that is keyed by that atom, or by its string equivalent.
+
+  If both atom and String keys exist in the map, the atom's value is returned.
+
+  ## Examples
+      iex> Swiss.Map.indif_fetch!(%{life: 42}, :life)
+      42
+
+      iex> Swiss.Map.indif_fetch!(%{"life" => 42}, :life)
+      42
+
+      iex> Swiss.Map.indif_fetch!(%{:life => 42, "life" => 64}, :life)
+      42
+
+      iex> Swiss.Map.indif_fetch!(%{}, :life)
+      ** (KeyError) key :life not found in: %{}
+  """
+  @spec indif_fetch!(Map.t(), atom()) :: any()
+  def indif_fetch!(map, key) when is_atom(key) do
+    Map.get_lazy(map, key, fn ->
+      string_key = Atom.to_string(key)
+
+      if Map.has_key?(map, string_key) do
+        map[string_key]
+      else
+        raise KeyError, "key #{inspect(key)} not found in: #{inspect(map)}"
+      end
+    end)
   end
 end
